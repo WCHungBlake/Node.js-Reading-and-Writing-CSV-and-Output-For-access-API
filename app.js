4d41b911df0ff79d395cb548777f6b108f3d9805
BlakeHung <mars7823482@gmail.com>
2022-02-14 09:41:07 +0800

    feat: execute function

--- app.js
+++ app.js
@@ -43,3 +43,66 @@ const csvWriter = createCsvWriter({
     {id: 'statusCode', title: '狀態碼'}
   ]
 });
+
+//丟入指定檔名即可使用
+async function analysis_map8(csvFile) {
+    try {
+        let writeData = [] 
+        const data = await getData(csvFile, {});
+        for(const [index, element] of data.entries()) {
+          let = value = {}
+          try {
+            //取得csv欄位資料
+            const userAddress = element.userAddress
+            const storeAddress = element.storeAddress
+            const distance_google = element.Distance
+            const time_google = element.Time
+            // 正規化使用者地址
+            const standardization_data_user = await axios.get(encodeURI('https://api.map8.zone/v2/address/standardization?query='+userAddress+'&key='key'))
+            if  (standardization_data_user.data.status === 'OK') {
+              const standardization_userAddress_address = standardization_data_user.data.results[0].formatted_address
+              //正規化前的地址做geocode
+              const content_before = await axios.get(encodeURI('http://api.map8.zone/distancematrix/foot/json?key='key'&origins='+storeAddress+'&destinations='+userAddress+'&language=zh_TW'))
+              //正規化後的地址做geocode
+              const content = await axios.get(encodeURI('http://api.map8.zone/distancematrix/foot/json?key='key'&origins='+storeAddress+'&destinations='+standardization_userAddress_address+'&language=zh_TW'))
+              //賦值
+              value = {
+                  userAddress: userAddress,
+                  storeAddress: storeAddress,
+                  userAddress_standardization: standardization_userAddress_address,
+                  time: time_google,
+                  distance_google: distance_google,
+                  map8_user_position: content.data.destinations[0].routePoint.location[0]+', '+content.data.destinations[0].routePoint.location[1],
+                  map8_level: content_before.data.destinations[0].place.level,
+                  auth: content_before.data.destinations[0].place.authoritative,
+                  distancn_map8_before: content_before.data.rows[0].elements[0].distance.value,
+                  distance_map8: content.data.rows[0].elements[0].distance.value,
+                  distance_google_map8_before: Number(distance_google) - Number(content_before.data.rows[0].elements[0].distance.value),
+                  distance_google_map8: Number(distance_google) - Number(content.data.rows[0].elements[0].distance.value),
+                  statusCode: standardization_data_user.data.results[0].resultAnalysis.statusCode
+              }
+            }
+            else {
+              value = {
+                userAddress: userAddress,
+                storeAddress: storeAddress,
+                time: time_google,
+                distance_google: distance_google,
+                statusCode: standardization_data_user.data.queryQuality.statusCode
+              }
+            }
+            //寫入資料
+            writeData.push(value)
+          }
+          catch (error) {
+            console.error("forLoop data ", error.message + element);
+          }
+        }
+        //輸出csv
+        csvWriter
+        .writeRecords(writeData)
+        .then(()=> console.log('The CSV file was written successfully'));
+    } catch (error) {
+        console.error("testGetData: An error occurred: ", error.message);
+    }
+  }
